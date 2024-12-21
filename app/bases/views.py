32351410b4a9b3a973d1c815b3d6446db3079ea5
ignxm <ignx.mfs@gmail.com>
2024-12-21 12:09:09 -0500

    new commit

--- app/bases/views.py
+++ app/bases/views.py
@@ -7,4 +7,4 @@ from django.views import generic
 class Home(LoginRequiredMixin, generic.TemplateView):
     # template_name = "base/base.html"
     template_name = "bases/home.html"
-    login_url = '/admin'
+    login_url = 'bases:login'#'/admin'
