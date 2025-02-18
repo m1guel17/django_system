18e06d964afb80177a16a05d672b1e5fb6718927
ignxm <ignx.mfs@gmail.com>
2025-02-17 20:09:46 -0500

    added redirect to forbidden page when no permissions 2

--- app/bases/views.py
+++ app/bases/views.py
@@ -18,7 +18,10 @@ class SinPrivilegios(PermissionRequiredMixin):
     raise_exception = False
     redirect_field_name = "redirect_to"
     def handle_no_permission(self): # this can be used to redirect to a 404/403 forbidden page
-        self.login_url = "bases:sin_privilegios"
+        from django.contrib.auth.models import AnonymousUser
+        if not self.request.user == AnonymousUser():
+            self.login_url = "bases:sin_privilegios"
+            
         return HttpResponseRedirect(reverse_lazy(self.login_url))
         
         """
